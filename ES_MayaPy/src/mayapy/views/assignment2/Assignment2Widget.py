c88768c58f5fc0dbef404567c8f702b5d16e3079
Eric Schultz <ericschultz101@gmail.com>
2015-12-03 16:15:53 -0800

    Added Slider Functionality

--- ES_MayaPy/src/mayapy/views/assignment2/Assignment2Widget.py
+++ ES_MayaPy/src/mayapy/views/assignment2/Assignment2Widget.py
@@ -3,6 +3,8 @@
 # Scott Ernst
 
 from pyglass.widgets.PyGlassWidget import PyGlassWidget
+import nimble
+from nimble import cmds
 
 #___________________________________________________________________________________________________ Assignment2Widget
 class Assignment2Widget(PyGlassWidget):
@@ -17,6 +19,11 @@ class Assignment2Widget(PyGlassWidget):
         super(Assignment2Widget, self).__init__(parent, **kwargs)
 
         self.homeBtn.clicked.connect(self._handleReturnHome)
+        self.eyes.sliderMoved.connect(self._handleEyes)
+        self.nose.sliderMoved.connect(self._handleNose)
+        self.mouth.sliderMoved.connect(self._handleMouth)
+        self.ears.sliderMoved.connect(self._handleEars)
+        self.forehead.sliderMoved.connect(self._handleForehead)
 
 #===================================================================================================
 #                                                                                 H A N D L E R S
@@ -27,15 +34,61 @@ class Assignment2Widget(PyGlassWidget):
 
 #___________________________________________________________________________________________________ _handleEyes
     def _handleEyes(self):
+        val = self.eyes.value()
+        val = float(val)/100
+        if val > 0:
+            cmds.blendShape( "blendShape1", edit=True, w=[(7, val)] )
+        if val < 0:
+            cmds.blendShape( "blendShape1", edit=True, w=[(6, -1 * val)] )
+        if val == 0:
+            cmds.blendShape( "blendShape1", edit=True, w=[(6, 0)] )
+            cmds.blendShape( "blendShape1", edit=True, w=[(7, 0)] )
+
 
 #___________________________________________________________________________________________________ _handleNose
     def _handleNose(self):
+        val = self.nose.value()
+        val = float(val)/100
+        if val > 0:
+            cmds.blendShape( "blendShape1", edit=True, w=[(3, val)] )
+        if val < 0:
+            cmds.blendShape( "blendShape1", edit=True, w=[(2, -1 * val)] )
+        if val == 0:
+            cmds.blendShape( "blendShape1", edit=True, w=[(2, 0)] )
+            cmds.blendShape( "blendShape1", edit=True, w=[(3, 0)] )
 
 #___________________________________________________________________________________________________ _handleMouth
     def _handleMouth(self):
+        val = self.mouth.value()
+        val = float(val)/100
+        if val > 0:
+            cmds.blendShape( "blendShape1", edit=True, w=[(5, val)] )
+        if val < 0:
+            cmds.blendShape( "blendShape1", edit=True, w=[(4, -1 * val)] )
+        if val == 0:
+            cmds.blendShape( "blendShape1", edit=True, w=[(5, 0)] )
+            cmds.blendShape( "blendShape1", edit=True, w=[(4, 0)] )
 
 #___________________________________________________________________________________________________ _handleEars
     def _handleEars(self):
+        val = self.ears.value()
+        val = float(val)/100
+        if val > 0:
+            cmds.blendShape( "blendShape1", edit=True, w=[(0, val)] )
+        if val < 0:
+            cmds.blendShape( "blendShape1", edit=True, w=[(1, -1 * val)] )
+        if val == 0:
+            cmds.blendShape( "blendShape1", edit=True, w=[(1, 0)] )
+            cmds.blendShape( "blendShape1", edit=True, w=[(0, 0)] )
 
 #___________________________________________________________________________________________________ _handleForehead
     def _handleForehead(self):
+        val = self.forehead.value()
+        val = float(val)/100
+        if val > 0:
+            cmds.blendShape( "blendShape1", edit=True, w=[(9, val)] )
+        if val < 0:
+            cmds.blendShape( "blendShape1", edit=True, w=[(8, -1 * val)] )
+        if val == 0:
+            cmds.blendShape( "blendShape1", edit=True, w=[(9, 0)] )
+            cmds.blendShape( "blendShape1", edit=True, w=[(8, 0)] )
